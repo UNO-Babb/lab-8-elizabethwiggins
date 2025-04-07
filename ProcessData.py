4aac0105ae78e57debc7de5a2f6862756095569c
elizabethwiggins <ewiggins@unomaha.edu>
2025-04-07 02:30:29 +0000

    break

--- ProcessData.py
+++ ProcessData.py
@@ -12,12 +12,42 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+ # line= inFile.readline()
+  for line in inFile:
+    data = line.split( )
+    #print(data)
+    
 
+    student_id=makeID(data[0], data[1], data[3])
+    major= data[6]
 
+    for line in data[5]:
+      if line== "Freshman":
+        year="FR"
+      if line== "Sophomore":
+        year="SR"
+      if line== "Junior":
+        year="JR"
+      if line=="Senior":
+        year="SR"
+
+      print(student_id, major[0:3]+"-"+year)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum,):
+  #print(first, last, idNum)
+  idLen= len(idNum)
+
+  while len(last) <5:
+    last= last + "X"
+
+  id= first[0] + last +idNum[idLen-3: ]
+  #print(id)
+  return id
+
 
 if __name__ == '__main__':
   main()
